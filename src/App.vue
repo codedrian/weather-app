189900724e5022f598b48eebf7d857f509a8dd83
Adrian Gaile Singh <adriangailee@gmail.com>
2024-06-21 19:01:43 +0800

    chore: Refactor App.vue to use grid layout for WeatherCard components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import SearchRegion from './assets/components/SearchRegion.vue'
 import WeatherCard from './assets/components/WeatherCard.vue'
+
 const places = ref([]);
+
 function displayForecast(data) {
-	places.value.push(data);
+  places.value.push(data);
 }
 
 </script>
 <template>
-	<SearchRegion @place-data="displayForecast"/>
-	<div class="grid grid-cols-2 gap-4">
-		<div v-for="(place, index) in places" :key="index">
-			<WeatherCard :place="place" />
-		</div>
-	</div>
+  <SearchRegion @place-data="displayForecast"/>
+  <div class="grid grid-cols-2 gap-4 mt-5">
+    <div v-for="(place, index) in places" :key="index">
+      <WeatherCard :place="place"/>
+    </div>
+  </div>
 </template>
