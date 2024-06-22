d1a2c5844e513fb19706b299cf1e0a9c3dd179bf
Adrian Gaile Singh <adriangailee@gmail.com>
2024-06-22 12:52:05 +0800

    refactor: Implement delete functionality for WeatherCard component

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,21 @@ function displayForecast(data) {
   places.value.push(data);
 }
 
+function deleteWeatherCard(name) {
+    /*TODO: Delete the index of the name*/
+    const weatherIndex = places.value.findIndex(place => place.location.name === name);
+    if (weatherIndex !== -1) {
+        places.value.splice(weatherIndex, 1); // Remove 1 element starting at weatherIndex
+    }
+    console.log(weatherIndex);
+}
+
 </script>
 <template>
   <SearchRegion @place-data="displayForecast"/>
   <div class="grid grid-cols-2 gap-4 mt-5">
     <div v-for="(place, index) in places" :key="index">
-      <WeatherCard :place="place"/>
+      <WeatherCard :place="place" @delete-weather-card="deleteWeatherCard"/>
     </div>
   </div>
 </template>
